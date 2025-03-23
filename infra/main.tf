829cd4ed222dc01f0ecc24c8367bfb98d5a7dc28
SousaPedroso <contato.gabrieldesousagp@gmail.com>
2025-03-22 20:59:37 -0400

    fix: incluir chave ssh existente na criação da máquina

--- infra/main.tf
+++ infra/main.tf
@@ -15,11 +15,15 @@ provider "digitalocean" {
     token = var.analytics_iac_droplet
 }
 
+data "digitalocean_ssh_key" "digital_oceans" {
+  name = "digital_oceans"
+}
+
 resource "digitalocean_droplet" "spotify_analytics" {
   image   = "ubuntu-20-04-x64"
   name    = "spotify-analytics"
   region  = "nyc2"
   size    = "s-1vcpu-1gb"
-
+  ssh_keys = [data.digitalocean_ssh_key.digital_oceans.id]
   tags = ["analytics", "spotify"]
 }
